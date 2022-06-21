edd7af6be0a2aade7797d889490bd894d24a7232
NQSH <thom.lecorre@gmail.com>
2022-06-21 12:49:38 +0200

    Removed frontmatter + Added gray-matter

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  webpack: (cfg) => {
-      cfg.module.rules.push(
-          {
-              test: /\.md$/,
-              loader: 'frontmatter-markdown-loader',
-              options: { mode: ['react-component'] }
-          }
-      )
-      return cfg;
-  }
-}
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      return config;
+    }
+  };
