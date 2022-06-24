3dd78672fecf91c7a4da9e2737f156fcc2b502d8
NQSH <thom.lecorre@gmail.com>
2022-06-24 21:26:47 +0200

    Added pricing display

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,8 +1,39 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function Pricing({}) {
+export default function Pricing({ services }) {
   return (
     <div style={{ flex: 1 }}>
       <h1>Pricing</h1>
+      {
+        services.map((service, index) => {
+          const { title, description, price } = service;
+          return (
+            <div key={index}>
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <b>{price}</b>
+            </div>
+          );
+        })
+      }
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("content/pricing"));
+
+  const services = files.map(file => {
+    const content = fs.readFileSync(path.join("content/pricing", file), "utf-8");
+    const { data } = matter(content);
+    return data;
+  })
+
+  return {
+    props: {
+      services
+    }
+  }
 }
