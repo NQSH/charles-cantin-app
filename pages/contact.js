fc63332e0e631608dd3a3f48c3327ce602847ba8
NQSH <thom.lecorre@gmail.com>
2022-06-25 00:30:09 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,5 @@
+import InputText from "@components/InputText";
+import InputTextArea from "@components/InputTextArea";
 
 export default function Contact({}) {
   return (
@@ -5,15 +7,9 @@ export default function Contact({}) {
       <h1>Contact</h1>
       <form name="contact" method="POST" data-netlify="true" action="/contact?status=success">
         <input type="hidden" name="form-name" value="contact" />
-        <div>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </div>
-        <div>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </div>
-        <div>
-          <label>Message: <textarea name="message"></textarea></label>
-        </div>
+        <InputText label={`Votre nom et prénom`} name={`name`} />
+        <InputText label={`Votre adresse e-mail`} name={`email`} />
+        <InputTextArea label={`Votre message`} name={`message`} />
         <div>
           <button type="submit">ENVOYER</button>
         </div>
