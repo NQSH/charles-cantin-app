42b726bde24bf7487f8b74b860401bfcae2493d5
NQSH <thom.lecorre@gmail.com>
2022-07-11 22:29:05 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,11 @@
-import React from "react";
-import NavBar from "./NavBar";
+import styles from "@styles/components/Layout.module.css";
+
+import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <NavBar />
+    <div className={styles.container}>
+      <Header />
       {children}
     </div>
   );
