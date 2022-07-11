5c086dfb5c0fde0704d980a5b301ea52d7132f91
NQSH <thom.lecorre@gmail.com>
2022-07-11 22:28:52 +0200

    Update Photo.js

--- components/Photo.js
+++ components/Photo.js
@@ -3,10 +3,12 @@ import Link from "next/link";
 
 export default function Photo({ photo: { image, slug }, disabled }) {
   return (
-    <div style={{ position: "relative", height: "100px", minWidth: "30%", flex: 1, display: !disabled ? "block" : "none" }}>
+    <div style={{ height: "100px", minWidth: "30%", flex: 1, display: !disabled ? "block" : "none" }}>
       <Link href={`gallery/${slug}`}>
         <a>
-          <Image src={`/${image}`} layout="fill" objectFit="cover" />
+          <div style={{ position: "relative", height: "100%", minWidth: "100%" }}>
+            <Image src={`/${image}`} layout="fill" objectFit="cover" placeholder="blur" blurDataURL={`/${image}`} />
+          </div>
         </a>
       </Link>
     </div>
