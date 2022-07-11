cddb45cccd04f4b0f35413b7e9b549d96b8092c4
NQSH <thom.lecorre@gmail.com>
2022-07-11 22:29:03 +0200

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,51 @@
-import Brand from "./Brand";
+import styles from "@styles/components/NavBar.module.css"
+
+import React from "react";
+
 import NavMenu from "./NavMenu";
 import NavSocial from "./NavSocial";
 
+import { useNavigation } from "context/navigation";
+
 export default function NavBar({}) {
+  const { currentMenu, handleSetCurrentMenu, menuList } = useNavigation();
+
+  const [markerPosition, setMarkerPosition] = React.useState();
+
   return (
-    <div style={{ flex: 1 }}>
-      <Brand />
-      <div>
-        <NavMenu href="/" label="Accueil" />
-        <NavMenu href="/gallery" label="Galerie" />
-        <NavMenu href="/pricing" label="Préstations" />
-        <NavMenu href="/contact" label="Contact" />
+    <div className={styles.container}>
+      <div
+        className={styles.menu_list_container}
+        data-iscurrenthome={currentMenu.id === "home"}
+      >
+        <ul className={styles.menu_list}>
+          {
+            menuList.map(menu => {
+              const { id } = menu;
+              return (
+                <NavMenu
+                  {...menu}
+                  onClickHandler={() => handleSetCurrentMenu(id)}
+                  isCurrent={currentMenu.id === id}
+                  setMarkerPosition={setMarkerPosition}
+                  key={id}
+                  isHomeCurrent={currentMenu.id === "home"}
+                />
+              );
+            })
+          }
+          <div
+            className={styles.menu_list_marker}
+            style={{ left: markerPosition }}
+            data-iscurrenthome={currentMenu.id === "home"}
+          />
+        </ul>
       </div>
-      <div>
-        <NavSocial src="/icon-social-facebook.png" />
-        <NavSocial src="/icon-social-instagram.png" />
+      <div className={styles.social_container}>
+        <div className={styles.social_wrapper}>
+          <NavSocial src="/icon-social-facebook.png" />
+          <NavSocial src="/icon-social-instagram.png" />
+        </div>
       </div>
     </div>
   );
