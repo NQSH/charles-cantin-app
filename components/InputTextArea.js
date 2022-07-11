74b5834434481e5149ed2eecdb1b512a8ec53ae4
NQSH <thom.lecorre@gmail.com>
2022-07-11 22:29:08 +0200

    Update InputTextArea.js

--- components/InputTextArea.js
+++ components/InputTextArea.js
@@ -2,7 +2,7 @@
 export default function InputTextArea({ label, name }) {
   return (
     <div style={{ flex: 1 }}>
-      <label for={name}>{label}</label>
+      <label htmlFor={name}>{label}</label>
       <textarea name={name} />
     </div>
   );
