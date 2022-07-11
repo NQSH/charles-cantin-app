600fe85d0154739fbcfb790dd4f9e5b678b26067
NQSH <thom.lecorre@gmail.com>
2022-07-11 22:28:58 +0200

    Update NavMenu.js

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -1,13 +1,35 @@
+import styles from "@styles/components/NavMenu.module.css";
+
+import React from "react";
 import Link from "next/link"
 
-export default function NavMenu({ href, label }) {
+export default function NavMenu({ href, label, onClickHandler, isCurrent, setMarkerPosition, isHomeCurrent }) {
+  const ref = React.useRef(null);
+  const [offset, setOffset] = React.useState();
+
+  React.useEffect(() => {
+    if(!offset) {
+      const value = ref.current.offsetLeft + ref.current.offsetWidth / 2 - 2;
+      setOffset(value);
+      isCurrent && setMarkerPosition(value);
+    } else {
+      isCurrent && setMarkerPosition(offset)
+    }
+  }, [isCurrent]);
+
   return (
-    <div style={{ flex: 1 }}>
+    <li className={styles.container}>
       <Link href={href}>
-        <a>
+        <a
+          onClick={onClickHandler}
+          className={styles.link}
+          data-iscurrent={isCurrent}
+          data-iscurrenthome={isHomeCurrent}
+          ref={ref}
+        >
           {label}
         </a>
       </Link>
-    </div>
+    </li>
   );
 }
