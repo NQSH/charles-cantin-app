6c32c869f5118f6724e9739037b1f5e2817083ea
NQSH <thom.lecorre@gmail.com>
2022-07-13 19:01:15 +0200

    Update FilterList.js

--- components/FilterList.js
+++ components/FilterList.js
@@ -1,13 +1,23 @@
 import styles from "@styles/components/FilterList.module.css"
 
-export default function FilterList({ title, filters, side, onClickHandler }) {
+export default function FilterList({ title, filters, side, onClickHandler, selectedFilters }) {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <ul className={styles.list}>
         {
           filters.map(filter => {
-            return <li className={styles.filter} onClick={() => onClickHandler(filter)}>{filter}</li>
+            return (
+              <li
+                className={styles.filter}
+                onClick={() => onClickHandler(filter)}
+                data-side={side}
+                data-isselected={selectedFilters.includes(filter)}
+                key={filter}
+              >
+                {filter}
+              </li>
+            );
           })
         }
       </ul>
