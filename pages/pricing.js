d21373136060de70a257391b463bc93ddb6257ad
NQSH <thom.lecorre@gmail.com>
2022-07-14 20:54:11 +0200

    Update pricing.js

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,34 +1,25 @@
+import styles from "@styles/pages/Pricing.module.css";
+
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import Link from "next/link";
+import Service from "@components/Service";
 
 export default function Pricing({ services }) {
   return (
-    <div style={{ flex: 1 }}>
-      <h1>Pricing</h1>
-      {
-        services.map((service, index) => {
-          const { title, description, price } = service;
-          return (
-            <Link
-              href={{
-                pathname: "/contact",
-                query: service
-              }}
-              key={index}
-            >
-              <a>
-                <div>
-                  <h2>{title}</h2>
-                  <p>{description}</p>
-                  <b>{price}</b>
-                </div>
-              </a>
-            </Link>
-          );
-        })
-      }
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        {
+          services.map((service, index) => {
+            return (
+              <Service
+                service={service}
+                key={index}
+              />
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
