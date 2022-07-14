ef77e4506f1d817a582506be5acb64d35ed95ea3
NQSH <thom.lecorre@gmail.com>
2022-07-14 18:13:36 +0200

    Update Brand.js

--- components/Brand.js
+++ components/Brand.js
@@ -1,4 +1,5 @@
 import styles from "@styles/components/Brand.module.css"
+
 import { useNavigation } from "context/navigation";
 
 import Link from "next/link";
@@ -11,8 +12,12 @@ export default function Brand({}) {
     <div className={styles.container} data-iscurrenthome={currentMenu.id === "home"}>
       <div className={styles.logo_container} data-iscurrenthome={currentMenu.id === "home"}>
         <Link href="/" >
-          <a onClick={() => handleSetCurrentMenu("home")}>
-            <Logo style={{ color: currentMenu.id === "home" ? "white": "black" }} />
+          <a
+            className={styles.link}
+            onClick={() => handleSetCurrentMenu("home")}
+            data-iscurrenthome={currentMenu.id === "home"}
+          >
+            <Logo style={{ color: "currentColor" }} />
           </a>
         </Link>
       </div>
