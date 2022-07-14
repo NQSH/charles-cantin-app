32988b9e2e7201c6bff26811b01870d01c86cd9a
NQSH <thom.lecorre@gmail.com>
2022-07-14 20:53:46 +0200

    Created files

---
+++ components/Service.js
@@ -0,0 +1,25 @@
+import styles from "@styles/components/Service.module.css";
+
+import Link from "next/link";
+
+export default function Service({ service }) {
+  const { title, description, price } = service;
+  return (
+    <div className={styles.container}>
+      <Link
+        href={{
+          pathname: "/contact",
+          query: title
+        }}
+      >
+        <a>
+          <div className={styles.wrapper}>
+            <h2 className={styles.title}>{title}</h2>
+            <b className={styles.price}>{price ? `${price}€` : "Sur mesure"}</b>
+            <p className={styles.description}>{description}</p>
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
+}
