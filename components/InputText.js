3bf0fab02e51605c95debde8e7a917c72675b4ae
NQSH <thom.lecorre@gmail.com>
2022-07-16 01:44:47 +0200

    Updated input components

--- components/InputText.js
+++ components/InputText.js
@@ -1,9 +1,38 @@
+import styles from "@styles/components/InputText.module.css";
 
-export default function InputText({ label, name }) {
+import React from "react";
+
+export default function InputText({ label, name, errorMessage, pattern }) {
+  const [isFocused, isFocusedChange] = React.useState(false);
+  const ref = React.useRef(null);
+
+  function handleChangeValue ({ target }) {
+    const { value } = target;
+    if(!value.match(pattern)) {
+      ref.current.setCustomValidity(errorMessage);
+    } else {
+      ref.current.setCustomValidity("");
+    }
+  }
+
   return (
-    <div style={{ flex: 1 }}>
-      <label htmlFor={name}>{label}</label>
-      <input type="text" name={name} />
+    <div className={styles.container}>
+      <label
+        className={styles.label}
+        data-isfocused={isFocused}
+        data-isempty={ref.current && ref.current.value === ""}
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      <input
+        className={styles.input}
+        type="text" name={name}
+        onFocus={() => isFocusedChange(true)}
+        onBlur={() => isFocusedChange(false)}
+        onChange={handleChangeValue}
+        ref={ref}
+      />
     </div>
   );
 }
--- components/InputTextArea.js
+++ components/InputTextArea.js
@@ -1,9 +1,29 @@
+import styles from "@styles/components/InputTextArea.module.css";
+
+import React from "react";
 
 export default function InputTextArea({ label, name }) {
+  const [isFocused, isFocusedChange] = React.useState(false);
+  const ref = React.useRef(null);
+
   return (
-    <div style={{ flex: 1 }}>
-      <label htmlFor={name}>{label}</label>
-      <textarea name={name} />
+    <div className={styles.container}>
+      <label
+        className={styles.label}
+        data-isfocused={isFocused}
+        data-isempty={ref.current && ref.current.value === ""}
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      <textarea
+        className={styles.input}
+        maxLength={500}
+        onFocus={() => isFocusedChange(true)}
+        onBlur={() => isFocusedChange(false)}
+        ref={ref}
+        required
+      ></textarea>
     </div>
   );
 }
