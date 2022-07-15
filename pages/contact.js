666d8363af6d46a76db3bddbc26f5d443e79d8bc
NQSH <thom.lecorre@gmail.com>
2022-07-15 18:52:35 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,19 +1,37 @@
+import styles from "@styles/pages/Contact.module.css";
+
 import InputText from "@components/InputText";
 import InputTextArea from "@components/InputTextArea";
 
 export default function Contact({}) {
   return (
-    <div style={{ flex: 1 }}>
-      <h1>Contact</h1>
-      <form name="contact" method="POST" data-netlify="true" action="/contact?status=success">
-        <input type="hidden" name="form-name" value="contact" />
-        <InputText label={`Votre nom et prénom`} name={`name`} />
-        <InputText label={`Votre adresse e-mail`} name={`email`} />
-        <InputTextArea label={`Votre message`} name={`message`} />
-        <div>
-          <button type="submit">ENVOYER</button>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <p className={styles.text}>
+          Pour toutes informations complémentaires ou demandes de RDV, merci de me contacter via le formulaire ci-dessous.
+        </p>
+        <form
+          className={styles.form}
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          action="/contact?status=success"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div>
+          <label>Your Name: <input type="text" name="name" /></label>
         </div>
-      </form>
+        <div>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </div>
+        <div>
+          <label>Message: <textarea name="message"></textarea></label>
+        </div>
+          <div>
+            <button type="submit">ENVOYER</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
