de78433afdda32f7ee195c1ee26527644d4ba7da
NQSH <thom.lecorre@gmail.com>
2022-07-16 03:24:09 +0200

    Update NavSocial.js

--- components/NavSocial.js
+++ components/NavSocial.js
@@ -1,11 +1,16 @@
 import styles from "@styles/components/NavSocial.module.css";
+
+import { useNavigation } from "context/navigation";
+
 import Link from "next/link";
 
 export default function NavSocial({ src }) {
+  const { handleSetCurrentMenu } = useNavigation();
+
   return (
     <div>
-      <Link href="/#">
-        <a className={styles.link}>
+      <Link href="/">
+        <a className={styles.link} onClick={() => handleSetCurrentMenu("home")}>
           <img src={src} width="50px" />
         </a>
       </Link>
