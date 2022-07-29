69f12484f74be38f25dc25017dd836c266acc143
NQSH <thom.lecorre@gmail.com>
2022-07-29 14:20:13 +0200

    Added responsive styles

--- components/FilterList.js
+++ components/FilterList.js
@@ -1,10 +1,22 @@
 import styles from "@styles/components/FilterList.module.css"
 
+import React from "react";
+
 export default function FilterList({ title, filters, side, onClickHandler, selectedFilters, deleteFiltersHandler }) {
+  const [isOpenned, isOpennedChange] = React.useState(false);
+
+  function handleToggleOpen() {
+    isOpennedChange(!isOpenned);
+    console.log("TEST")
+  }
+
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>{title}</h2>
-      <ul className={styles.list}>
+      <h2 className={styles.title} onClick={handleToggleOpen}>
+        {title}
+        <div className={styles.title_arrow} />
+      </h2>
+      <ul className={styles.list} data-isopenned={isOpenned}>
         {
           filters.map(filter => {
             return (
