c32fbfd973991af394b94b144635d6b21deb6c67
NQSH <thom.lecorre@gmail.com>
2022-10-26 18:39:07 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,12 +5,15 @@ import Footer from "./Footer";
 import Head from "next/head";
 
 import { AnimatePresence } from "framer-motion";
+import { useNavigation } from "context/navigation";
 
 export default function Layout({ children }) {
+  const { currentMenu } = useNavigation();
   return (
     <div className={styles.container}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{`Charles Cantin - ${currentMenu.label}`}</title>
       </Head>
       <Header />
       <AnimatePresence exitBeforeEnter>
